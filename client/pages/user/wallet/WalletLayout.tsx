48dae13d8db6eaafc295276aa6a765736040460e
dito <itodaisuke00@gmail.com>
2023-04-14 23:17:15 +0900

    feat: social login

--- client/pages/user/wallet/WalletLayout.tsx
+++ client/pages/user/wallet/WalletLayout.tsx
@@ -1,9 +1,3 @@
-import { ReactNode } from "react";
-
-type WalletLayoutProps = {
-  children: ReactNode;
-};
-
 import { ChakraProvider, Flex, Heading, Text } from "@chakra-ui/react";
 import {
   ConnectButton,
@@ -14,6 +8,20 @@ import {
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import {
+  metaMaskWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import {
+  discordWallet,
+  enhanceWalletWithAAConnector,
+  facebookWallet,
+  githubWallet,
+  googleWallet,
+  twitchWallet,
+  twitterWallet,
+} from "@zerodevapp/wagmi/rainbowkit";
+import { ReactNode } from "react";
+import {
   WagmiConfig,
   configureChains,
   createClient,
@@ -22,12 +30,37 @@ import {
 } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 
-const appName = "EVM Hackathon Starter";
+const projectId = "14507bac-5aad-47ba-8b9a-66d745436246";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [...(process.env.NEXT_PUBLIC_TESTNET === "true" ? [goerli] : [mainnet])],
   [publicProvider()]
 );
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Social",
+    wallets: [
+      googleWallet({ options: { projectId } }),
+      facebookWallet({ options: { projectId } }),
+      githubWallet({ options: { projectId } }),
+      discordWallet({ options: { projectId } }),
+      twitchWallet({ options: { projectId } }),
+      twitterWallet({ options: { projectId } }),
+    ],
+  },
+  {
+    groupName: "Web3 Wallets (AA-enabled)",
+    wallets: [
+      enhanceWalletWithAAConnector(metaMaskWallet({ chains }), { projectId }),
+      enhanceWalletWithAAConnector(walletConnectWallet({ chains }), {
+        projectId,
+      }),
+    ],
+  },
+]);
+
+const appName = "EVM Hackathon Starter";
 
 const { wallets } = getDefaultWallets({
   appName,
@@ -38,14 +71,16 @@ const demoAppInfo = {
   appName,
 };
 
-const connectors = connectorsForWallets(wallets);
-
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
 });
+
+type WalletLayoutProps = {
+  children: ReactNode;
+};
 
 const WalletLayout = ({ children }: WalletLayoutProps) => {
   return (
