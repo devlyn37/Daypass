6732706b0b9d4dc7da3d098c4516f5034cecab0a
dito <itodaisuke00@gmail.com>
2023-04-14 23:06:47 +0900

    transpile @zerodevapp

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,15 @@
+const withTM = require("next-transpile-modules")([
+  "@zerodevapp/wagmi",
+  "@zerodevapp/social-wallet",
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withTM({
+  ...nextConfig,
+  webpack5: true,
+});
