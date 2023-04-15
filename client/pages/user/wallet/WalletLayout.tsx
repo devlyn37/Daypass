76bcb27dbed9552951a401481f79fb6b91323fce
syahrul <muhdsyahrulnizam123@gmail.com>
2023-04-15 18:01:07 +0800

    Add swap page

--- client/pages/user/wallet/WalletLayout.tsx
+++ client/pages/user/wallet/WalletLayout.tsx
@@ -1,4 +1,7 @@
-import { ChakraProvider, Flex, Heading, Text } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Heading } from "@chakra-ui/react";
+import { Image as ChakraImage } from '@chakra-ui/image';
+import Head from 'next/head';
+
 import {
   ConnectButton,
   RainbowKitProvider,
@@ -85,6 +88,16 @@ type WalletLayoutProps = {
 const WalletLayout = ({ children }: WalletLayoutProps) => {
   return (
     <WagmiConfig client={wagmiClient}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <RainbowKitProvider
         appInfo={demoAppInfo}
         chains={chains}
@@ -95,20 +108,24 @@ const WalletLayout = ({ children }: WalletLayoutProps) => {
         <ChakraProvider>
           <Flex
             minH="100vh"
-            margin={10}
+            padding="1vw"
             flexDirection="column"
             alignItems="stretch"
             justifyContent="space-between"
           >
-            <Flex justifyContent="space-between" alignItems="center">
-              <Heading>Hackathon</Heading>
-              <ConnectButton />
+            <Flex align="center">
+              <ChakraImage src="logo.png" alt="Your Image" boxSize="50px" marginRight="1rem" />
+              <Heading
+                fontFamily="'Righteous', sans-serif"
+                fontStyle="normal"
+                fontWeight="400"
+                fontSize="25.8683px"
+                lineHeight="101%"
+                letterSpacing="-0.022em"
+                color="#FF44EC">Space Can</Heading>
             </Flex>
             <Flex flexGrow={1}>{children}</Flex>
             <Flex justifyContent="space-between" alignItems="center">
-              <Heading>Hackathon</Heading>
-              <Text>Hi</Text>
-              <Text>Hi</Text>
             </Flex>
           </Flex>
         </ChakraProvider>
