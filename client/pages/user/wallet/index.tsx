9bbf702d1072f0877e874b0d58cd31481aae0a9f
dito <itodaisuke00@gmail.com>
2023-04-16 02:09:01 +0900

    render owned Daypass

--- client/pages/user/wallet/index.tsx
+++ client/pages/user/wallet/index.tsx
@@ -1,9 +1,40 @@
+import { Card, Image } from "@chakra-ui/react";
 import { useAccount } from "wagmi";
 import WalletLayout from "./WalletLayout";
+import useNFTs from "./useNFTs";
 
 const WalletPage = () => {
   const { address } = useAccount();
-  return <WalletLayout>{address && <div> connected! </div>}</WalletLayout>;
+
+  const NFT_CONTRACT_ADDRESS = "0xf03C1cB42c64628DE52d8828D534bFa2c6Fd65Df";
+  const nfts = useNFTs(NFT_CONTRACT_ADDRESS, address as string);
+
+  if (!address) {
+    return (
+      <WalletLayout>
+        <></>
+      </WalletLayout>
+    );
+  }
+
+  return (
+    <WalletLayout>
+      <div>
+        {nfts.map((nft) => (
+          <Card p="4" m="4" key={nft.tokenId}>
+            <div>Token ID: {nft.tokenId}</div>
+            <div>Name: {nft.name}</div>
+            <Image
+              src={nft.image}
+              alt={nft.name}
+              borderRadius="lg"
+              border="1px"
+            />
+          </Card>
+        ))}
+      </div>
+    </WalletLayout>
+  );
 };
 
 export default WalletPage;
---
+++ client/pages/user/wallet/useNFTs.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { useProvider } from "wagmi";
+import abi from "./../../../contracts/Hackathon721.sol/Hackathon721.json";
+
+interface NFT {
+  tokenId: string;
+  name: string;
+  image: string;
+  tokenURI: string;
+}
+
+export default function useNFTs(
+  contractAddress: string,
+  walletAddress: string
+): NFT[] {
+  const [nfts, setNFTs] = useState<NFT[]>([]);
+  const provider = useProvider();
+
+  useEffect(() => {
+    const fetchNFTs = async () => {
+      const contract = new ethers.Contract(contractAddress, abi.abi, provider);
+
+      const balance = await contract.balanceOf(walletAddress);
+
+      const tokenIds = [];
+      for (let i = 0; i < balance; i++) {
+        const tokenId = await contract.tokenOfOwnerByIndex(walletAddress, i);
+        tokenIds.push(tokenId.toString());
+      }
+
+      const nftPromises = tokenIds.map(async (tokenId) => {
+        const tokenURI = await contract.tokenURI(tokenId);
+
+        const metadata = await axios.get(tokenURI);
+        const { name, image } = metadata.data;
+        return {
+          tokenId,
+          tokenURI,
+          name,
+          image,
+        };
+      });
+
+      const nftData = await Promise.all(nftPromises);
+      setNFTs(nftData);
+    };
+
+    fetchNFTs();
+  }, [contractAddress, walletAddress]);
+
+  return nfts;
+}
