6d4a98d38dd8a457bf0cecba89189d70be1e6cc7
dito <itodaisuke00@gmail.com>
2023-04-16 02:41:40 +0900

    fix: use eoa on admin page

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import AAWalletProvider from "../providers/aaWallet";
 import WalletProvider from "../providers/wallet";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <WalletProvider>
-    <Component {...pageProps} />
-  </WalletProvider>
+  const router = useRouter();
+  // if path is /admin, use WalletProvider
+  if (router.pathname.startsWith("/admin")) {
+    return (
+      <WalletProvider>
+        <Component {...pageProps} />
+      </WalletProvider>
+    );
+  }
+
+  return (
+    <AAWalletProvider>
+      <Component {...pageProps} />
+    </AAWalletProvider>
+  );
 }
