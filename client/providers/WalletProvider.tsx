9d0896a7d72bbb638f97adca7536628b095b433e
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-04-23 18:00:05 +0900

    change zero dev projectId and wagmiclient based on chain

--- client/providers/WalletProvider.tsx
+++ client/providers/WalletProvider.tsx
@@ -14,45 +14,20 @@ import {
   googleWallet,
   twitterWallet,
 } from "@zerodevapp/wagmi/rainbowkit";
-import { ReactNode } from "react";
-import {
-  WagmiConfig,
-  configureChains,
-  createClient,
-  goerli,
-  mainnet,
-} from "wagmi";
-import { polygon, polygonMumbai } from "wagmi/chains";
+import { ReactNode, useMemo } from "react";
+import { WagmiConfig, configureChains, createClient, useNetwork } from "wagmi";
+import { polygonMumbai, goerli } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
-const projectId = (() => {
-  switch (process.env.NEXT_PUBLIC_CHAIN!) {
-    case "ethereum":
-      return "14507bac-5aad-47ba-8b9a-66d745436246";
-    case "polygon":
-      return "d00b56eb-3a21-44d7-803a-b7cca690fe6a";
-  }
-
-  return "14507bac-5aad-47ba-8b9a-66d745436246";
-})();
+const supported_chains = [goerli, polygonMumbai];
+const goerliZeroDevProjectId = "14507bac-5aad-47ba-8b9a-66d745436246";
+const chainToZeroDevProjectId = new Map<number, string>([
+  [5, goerliZeroDevProjectId],
+  [80001, "d00b56eb-3a21-44d7-803a-b7cca690fe6a"],
+]);
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [
-    ...((): any[] => {
-      switch (process.env.NEXT_PUBLIC_CHAIN!) {
-        case "ethereum":
-          return process.env.NEXT_PUBLIC_TESTNET === "true"
-            ? [goerli]
-            : [mainnet];
-        case "polygon":
-          return process.env.NEXT_PUBLIC_TESTNET === "true"
-            ? [polygonMumbai]
-            : [polygon];
-      }
-
-      return process.env.NEXT_PUBLIC_TESTNET === "true" ? [goerli] : [mainnet];
-    })(),
-  ],
+  supported_chains,
   [publicProvider()]
 );
 
@@ -61,39 +36,40 @@ const demoAppInfo = {
   appName,
 };
 
-const connectors = connectorsForWallets([
-  {
-    groupName: "Social Smart Accounts",
-    wallets: [
-      googleWallet({ options: { projectId } }),
-      githubWallet({ options: { projectId } }),
-      twitterWallet({ options: { projectId } }),
-    ],
-  },
-  {
-    groupName: "Standard Wallets",
-    wallets: [
-      metaMaskWallet({ chains }),
-      rainbowWallet({ chains }),
-      walletConnectWallet({ chains }),
-    ],
-  },
-]);
+const getWagmiClient = (zeroDevProjectId: string) =>
+  createClient({
+    autoConnect: true,
+    connectors: connectorsForWallets([
+      {
+        groupName: "Social Smart Accounts",
+        wallets: [
+          googleWallet({ options: { projectId: zeroDevProjectId } }),
+          githubWallet({ options: { projectId: zeroDevProjectId } }),
+          twitterWallet({ options: { projectId: zeroDevProjectId } }),
+        ],
+      },
+      {
+        groupName: "Standard Wallets",
+        wallets: [
+          metaMaskWallet({ chains }),
+          rainbowWallet({ chains }),
+          walletConnectWallet({ chains }),
+        ],
+      },
+    ]),
+    provider,
+    webSocketProvider,
+  });
+const defaultWagmiClient = getWagmiClient(goerliZeroDevProjectId);
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-  webSocketProvider,
-});
+const WalletProvider = ({ children }: { children: ReactNode }) => {
+  const { chain } = useNetwork();
+  const chainId = chain?.id || goerli.id;
+  const id = chainToZeroDevProjectId.get(chainId) ?? goerliZeroDevProjectId;
+  const wagmiClient = useMemo(() => getWagmiClient(id), [chain]);
 
-type WalletLayoutProps = {
-  children: ReactNode;
-};
-
-const WalletProvider = ({ children }: WalletLayoutProps) => {
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig client={wagmiClient ?? defaultWagmiClient}>
       <RainbowKitProvider
         appInfo={demoAppInfo}
         chains={chains}
